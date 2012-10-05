6a856ccd49da02d02c8a9d22083ff8a771ab9848
Joseph Bergen <jbergen@gmail.com>
2012-10-04 20:50:19 -0400

    updated docs

--- docs/api.js
+++ docs/api.js
@@ -3,21 +3,7 @@ YUI.add("yuidoc-meta", function(Y) {
     "classes": [
         "Player"
     ],
-    "modules": [
-        "widget",
-        "widget-foo"
-    ],
-    "allModules": [
-        {
-            "displayName": "widget",
-            "name": "widget",
-            "description": "Provides more functionality for the widget module.."
-        },
-        {
-            "displayName": "widget-foo",
-            "name": "widget-foo",
-            "description": "Provides more functionality for the widget module.."
-        }
-    ]
+    "modules": [],
+    "allModules": []
 } };
 });
