511d0be54daf02667bf2db2601f15e4bd9d31c27
Joseph Bergen <jbergen@gmail.com>
2012-12-04 13:57:40 -0500

    release

--- dist/debug/templates.js
+++ dist/debug/templates.js
@@ -67,11 +67,11 @@ var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
 __p+='<a href=\'#\' class=\'arrow arrow-left slideshow-slider-control-prev\'></a>\n<a href=\'#\' class=\'arrow arrow-right slideshow-slider-control-next\'></a>\n\n<ul>\n\t';
  _.each(attr.slides, function(slide, i){ 
-;__p+='\n\t\t<li>\n\t\t\t<a href=\'#\' class=\'slider-thumb\' data-slidenum="'+
-( i )+
-'">\n\t\t\t\t<div class=\'slideshow-thumbnail\' style="background:url('+
+;__p+='\n\t\t<li>\n\t\t\t<div class=\'slideshow-thumbnail\' style="background:url('+
 ( slide.attr.uri )+
-'); background-repeat:no-repeat;background-size:100%;background-position:center"></div>\n\t\t\t\t<div class=\'thumb-title\'>\n\t\t\t\t\t<a href="'+
+'); background-repeat:no-repeat;background-size:100%;background-position:center">\n\t\t\t\t<a href=\'#\' class=\'slider-thumb\' data-slidenum="'+
+( i )+
+'"></a>\n\t\t\t\t<div class=\'thumb-title\'>\n\t\t\t\t\t<a href="'+
 ( slide.attr.attribution_uri )+
 '" target=\'blank\'>\n\t\t\t\t\t\t';
  if(slide.attr.media_creator_username.replace(/\s+/g, '') != ''){ 
@@ -83,7 +83,7 @@ __p+='<a href=\'#\' class=\'arrow arrow-left slideshow-slider-control-prev\'></a
  } 
 ;__p+='\n\t\t\t\t\t\t<i class=\'slideshow-icon-'+
 ( slide.attr.archive.toLowerCase() )+
-' ssarchive\'></i>\n\t\t\t\t\t</a>\n\t\t\t\t</div>\n\t\t\t</a>\n\t\t\t\n\t\t</li>\n\t';
+' ssarchive\'></i>\n\t\t\t\t\t</a>\n\t\t\t\t</div>\n\t\t\t</div>\t\t\t\n\t\t</li>\n\t';
  });
 ;__p+='\n</ul>';
 }
