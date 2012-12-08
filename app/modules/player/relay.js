ca00a3921e490bde01e019697af8ea1b5e58d689
Joseph Bergen <jbergen@gmail.com>
2012-12-08 18:40:31 -0500

    rm log and added comment

--- app/modules/player/relay.js
+++ app/modules/player/relay.js
@@ -2,6 +2,10 @@
 	relay.js
 
 	model that keeps track of player state, emits events, and relays commands to the player
+
+	the same relay object lives in all frames and layers and can be listened to by any of them
+
+	primarily used to relay commands from a layer to the project
 */
 
 define([
