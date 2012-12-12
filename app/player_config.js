1a82daa92355a56ca6b8f4e2ea8ccc07cd52ec61
Rick Waldron <waldron.rick@gmail.com>
2012-12-12 14:20:23 -0500

    Indents to 4 spaces: app/player_config.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/player_config.js
+++ app/player_config.js
@@ -1,31 +1,31 @@
 // Set the require.js configuration for your application.
 require.config({
 
-	// Initialize the application with the main application file.
-	deps: [ "main" ],
+    // Initialize the application with the main application file.
+    deps: [ "main" ],
 
-	paths: {
-		// JavaScript folders.
-		libs: "../assets/js/libs",
-		plugins: "../assets/js/plugins",
-		vendor: "../assets/vendor",
-		zeega_dir: "../app/modules",
-		
-		// Libraries.
-		//jquery: "../assets/js/libs/jquery",
-		lodash: "../assets/js/libs/lodash",
-		backbone: "../assets/js/libs/backbone"
+    paths: {
+        // JavaScript folders.
+        libs: "../assets/js/libs",
+        plugins: "../assets/js/plugins",
+        vendor: "../assets/vendor",
+        zeega_dir: "../app/modules",
 
-	},
-	shim: {
-		// Backbone library depends on lodash and jQuery.
-		backbone: {
-			deps: ["lodash"],
-			exports: "Backbone"
-		},
+        // Libraries.
+        //jquery: "../assets/js/libs/jquery",
+        lodash: "../assets/js/libs/lodash",
+        backbone: "../assets/js/libs/backbone"
 
-		// Backbone.LayoutManager depends on Backbone.
-		"plugins/backbone.layoutmanager": ["backbone"]
-	}
+    },
+    shim: {
+        // Backbone library depends on lodash and jQuery.
+        backbone: {
+            deps: [ "lodash" ],
+            exports: "Backbone"
+        },
+
+        // Backbone.LayoutManager depends on Backbone.
+        "plugins/backbone.layoutmanager": [ "backbone" ]
+    }
 
 });
