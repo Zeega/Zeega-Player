0981944a845ef6c2e8b03d105df1db5bd92019fe
Rick Waldron <waldron.rick@gmail.com>
2012-12-12 19:17:12 -0500

    Indents to 4 spaces, double quotes, etc: app/modules/player/helper.js
    Includes minor refactors
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/modules/player/helpers.js
+++ app/modules/player/helpers.js
@@ -1,100 +1,91 @@
-define([ "zeega" ],
+define([
+    "zeega"
+],
 
-function(Zeega)
-{
+function( Zeega ) {
 
-	var Helpers = Zeega.module();
+    var Helpers = Zeega.module(),
+        location = window.location;
 
-	Helpers.getHost = function()
-	{
-		return  window.location.protocol + "//" + window.location.host;        
-	};
+    Helpers.getHost = function() {
+        return  location.protocol + "//" + location.host;
+    };
 
-	Helpers.getBaseURL = function()
-	{
-		var currPath = window.location.pathname.split("/");        
-		var splitIdx = currPath.indexOf("/web/");        
-		var urlPath = window.location.pathname.substring(0,splitIdx+4);
-		return urlPath;
-	};
+    Helpers.getBaseURL = function() {
+        var currPath = location.pathname.split("/");
+        var splitIdx = currPath.indexOf("/web/");
+        var urlPath = location.pathname.substring( 0, splitIdx + 4 );
+        return urlPath;
+    };
 
-	Helpers.convertTime = function(seconds)
-	{
-		var m=Math.floor(seconds/60);
-		var s=Math.floor(seconds%60);
-		if(s<10) s="0"+s;
-		return m+":"+s;
-	};
+    Helpers.convertTime = function( seconds ) {
+        var m = Math.floor( seconds / 60 );
+        var s = Math.floor( seconds % 60 );
+        if ( s < 10 ) {
+            s = "0" + s;
+        }
+        return m + ":" + s;
+    };
 
-	Helpers.convertTime = function(seconds)
-	{
-		var m=Math.floor(seconds/60);
-		var s=Math.floor(seconds%60);
-		if(s<10) s="0"+s;
-		return m+":"+s;
-	};
+    Helpers.deconvertTime = function( minutes, seconds ) {
+        return 60 * minutes + parseInt( seconds , 10 );
+    };
 
-	Helpers.deconvertTime = function(minutes,seconds)
-	{
-		return 60 * minutes + parseInt( seconds , 10);
-	};
+    Helpers.getMinutes = function( seconds ) {
+        return Math.floor( parseInt( seconds, 10 ) / 60.0 );
+    };
 
-	Helpers.getMinutes = function(seconds)
-	{
-		return Math.floor( parseInt( seconds , 10 ) / 60.0 );
-	};
+    Helpers.getSeconds = function( seconds ) {
+        var s = Math.floor( (seconds % 60) * 10 ) / 10.0;
+        if ( s < 10 ) {
+            s = "0" + s;
+        }
+        return s;
+    };
 
-	Helpers.getSeconds = function(seconds)
-	{
-		var s=Math.floor((seconds%60)*10)/10.0;
-		if(s<10) s="0"+s;
-		return s;
-	};
+    Helpers.isInt = function( x ) {
+        return !isNaN( parseFloat( x ) ) && isFinite( x );
+    };
 
-	Helpers.isInt = function(x)
-	{
-		var y = parseInt(x,10);
-		if (isNaN(y)) return false;
-		return x==y && x.toString()==y.toString();
-	};
+    Helpers.isPercent = function(x) {
+        return isInt(x) && parseInt( x, 10 ) <= 100;
+    };
 
-	Helpers.isPercent = function(x)
-	{
-		return isInt(x) && parseInt(x,10) <= 100;
-	};
+    // TODO: Find actual uses of this function. Any in existance can't
+    // possibly work correctly since there is no "toHex" global function
+    Helpers.rgbToHex = function( R, G, B ) {
+        return toHex( R ) + toHex( G ) + toHex( B );
+    };
 
-	Helpers.rgbToHex = function(R,G,B)
-	{
-		return toHex(R)+toHex(G)+toHex(B);
-	};
+    var hexChars = "0123456789ABCDEF";
+    Helpers.toHex = function( n ) {
+        n = parseInt( n, 10 );
 
-	Helpers.toHex = function(n)
-	{
-		n = parseInt(n,10);
-		if (isNaN(n)) return "00";
-		n = Math.max(0,Math.min(n,255));
-		return "0123456789ABCDEF".charAt((n-n%16)/16) + "0123456789ABCDEF".charAt(n%16);
-	};
+        if ( isNaN(n) ) {
+            return "00";
+        }
+        n = Math.max( 0, Math.min(n, 255) );
 
-	// make capital case
-	String.prototype.toCapitalCase = function()
-	{
-		return this.charAt(0).toUpperCase() + this.substring(1).toLowerCase();
-	};
+        return hexChars.charAt( (n - n % 16) / 16 ) + hexChars.charAt( n % 16 );
+    };
 
-	String.prototype.toRGB = function()
-	{
-		var cutHex = function(h){ return (h.charAt(0)=="#") ? h.substring(1,7) : h ;};
-		var rgb = '';
-		rgb += parseInt( ( cutHex(this) ).substring( 0,2 ) , 16 ) +',';
-		rgb += parseInt( ( cutHex(this) ).substring( 2,4 ) , 16 ) +',';
-		rgb += parseInt( ( cutHex(this) ).substring( 4,6 ) , 16 );
-		return rgb;
-	};
+    // make capital case
+    String.prototype.toCapitalCase = function() {
+        return this.charAt(0).toUpperCase() + this.substring(1).toLowerCase();
+    };
 
-	// Required, return the module for AMD compliance
-	return Helpers;
+    String.prototype.toRGB = function() {
+        var str = this.replace(/^#/, "");
 
+        return [
+            parseInt( str.substring( 0, 2 ) , 16 ),
+            parseInt( str.substring( 2, 4 ) , 16 ),
+            parseInt( str.substring( 4, 6 ) , 16 )
+        ].join(",");
+    };
+
+    // Required, return the module for AMD compliance
+    return Helpers;
 });
 
 
@@ -106,6 +97,6 @@ function(Zeega)
 
 // function addGlobal(layerId,event,elementId)
 // {
-//		if(!LayerGlobals[layerId])LayerGlobals[layerId]={};
-//		eval('LayerGlobals[layerId].'+event+'= function(data){$("#'+elementId+'").trigger("'+event+'",data);}');
+//      if(!LayerGlobals[layerId])LayerGlobals[layerId]={};
+//      eval('LayerGlobals[layerId].'+event+'= function(data){$("#'+elementId+'").trigger("'+event+'",data);}');
 // }
