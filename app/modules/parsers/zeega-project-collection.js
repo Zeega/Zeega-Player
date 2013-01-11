a1a35a09dad6934bda6da9c9649f339c7952fe8a
James Burns <jameburn@gmail.com>
2013-01-11 17:43:35 -0500

    add zeega project collection parser

---
+++ app/modules/parsers/zeega-project-collection.js
@@ -0,0 +1,50 @@
+define([
+    "lodash"
+],
+function() {
+    var type = "zeega-project-collection",
+        Parser = {};
+
+    Parser[ type ] = { name: type };
+
+    Parser[ type ].validate = function( response ) {
+        if ( response.items && response.items.length>1 ) {
+            _.each(response.items,function(item){
+                if(item.media_type!="project"){
+                    return false;
+                }
+            });
+            return true;
+        }
+        return false;
+    };
+
+    Parser[ type ].parse = function( response, opts ) {
+        
+        var project = {
+
+            title : "Project Collection",
+            sequences : [],
+            frames : [],
+            layers : []
+        };
+
+        _.each(response.items, function(item){
+            project.layers = _.union(project.layers,item.text.layers);
+            project.frames = _.union(project.frames,item.text.frames);
+            if(project.sequences.length>0){
+                console.log(item);
+                project.sequences[project.sequences.length-1].advance_to=item.text.sequences[0].id;
+            }
+            project.sequences = _.union(project.sequences,item.text.sequences);
+
+        });
+
+        console.log("PROJECT",project);
+        return project;
+    };
+
+
+
+    return Parser;
+});
