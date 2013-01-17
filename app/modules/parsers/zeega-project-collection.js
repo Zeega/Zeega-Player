a3461e37b5a559505d9f83f1d27ebc8664d7f025
Joseph Bergen <jbergen@gmail.com>
2013-01-17 18:47:19 -0500

    fixed zeega project collection parse logic and style

--- app/modules/parsers/zeega-project-collection.js
+++ app/modules/parsers/zeega-project-collection.js
@@ -8,41 +8,33 @@ function() {
     Parser[ type ] = { name: type };
 
     Parser[ type ].validate = function( response ) {
-        if ( response.items && response.items.length>1 ) {
-            _.each(response.items,function(item){
-                if(item.media_type!="project"){
-                    return false;
-                }
-            });
-            return true;
+        if ( response.items && response.items.length > 1 ) {
+            var mediaTypes = _.pluck( response.items, "media_type" );
+                containsAllProjects = _.contains( mediaTypes, "project" );
+
+            if ( containsAllProjects ) {
+                return true;
+            }
         }
         return false;
     };
 
     Parser[ type ].parse = function( response, opts ) {
-        
-
-
         var project = {
-
             title : response.request.query.tags,
             sequences : [],
             frames : [],
             layers : []
         };
-
-        _.each(response.items, function(item){
-            project.layers = _.union(project.layers,item.text.layers);
-            project.frames = _.union(project.frames,item.text.frames);
-            if(project.sequences.length>0){
-                console.log(item);
-                project.sequences[project.sequences.length-1].advance_to=item.text.sequences[0].id;
+        
+        _.each( response.items, function( item ){
+            project.layers = _.union( project.layers, item.text.layers );
+            project.frames = _.union( project.frames, item.text.frames );
+            if( project.sequences.length > 0 ) {
+                project.sequences[ project.sequences.length - 1 ].advance_to = item.text.sequences[0].id;
             }
-            project.sequences = _.union(project.sequences,item.text.sequences);
-
+            project.sequences = _.union( project.sequences, item.text.sequences );
         });
-
-        console.log("PROJECT",project);
         return project;
     };
 
