129859c23a1e34b15ff6d26ca1d4cb6ab5339e6d
Joseph Bergen <jbergen@gmail.com>
2013-01-28 17:54:14 -0500

    updated parser

--- grunt.js
+++ grunt.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
         server: {
             files: {
                 "favicon.ico": "favicon.ico",
-                "testdata/test.json": "testdata/79007.json",
+                "testdata/test.json": "testdata/46384.json",
                 "example-data.js": "example-data.js"
             },
 
