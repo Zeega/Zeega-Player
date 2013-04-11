d823bc2a3b1a2dc285d5006cb5648a6069fdd4bc
jbergen <jbergen@gmail.com>
2013-04-10 17:20:29 -0700

    updated getProjectData to return reduced layer set

--- app/modules/player.js
+++ app/modules/player.js
@@ -581,24 +581,35 @@ function( Zeega, ZeegaParser, Relay, Status, PlayerLayout, Parse ) {
         // TODO: update this
         // returns project metadata
         getProjectData: function() {
-            var frames = [];
+            var frames = [],
+                layers = [];
 
             this.project.sequences.each(function( sequence ) {
                 sequence.frames.each(function( frame ) {
-                    var f = _.extend({},
+                    var l, f;
+
+                    l = frame.layers.toJSON();
+                    f = _.extend({},
                         frame.toJSON(),
-                        { layers: frame.layers.toJSON() }
+                        { layers: l }
                     );
 
+                    layers.push( l );
                     frames.push( f );
                 });
+            });
+
+            layers = _.flatten( layers );
+            layers = _.uniq( layers, function( lay) {
+                return lay.id;
             });
 
             return _.extend({},
                 this.toJSON(),
                 {
                     sequences: this.project.sequences.toJSON(),
-                    frames: frames
+                    frames: frames,
+                    layers: layers
                 }
             );
         },
