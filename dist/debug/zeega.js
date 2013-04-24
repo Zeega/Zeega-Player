2385a94b9b7bde02e056749ef9991ab2882639d2
jbergen <jbergen@gmail.com>
2013-04-24 15:49:57 -0700

    updated parser

--- dist/debug/zeega.js
+++ dist/debug/zeega.js
@@ -34154,41 +34154,57 @@ function( app, _Layer, Visual ){
             return this.audio;
         },
 
-        listen: function() {
-            jQuery(this.audio).on("canplay", function() {
-                this.model.trigger( "visual_ready", this.model.id );
-                this.model.trigger( "canplay", this.model );
-                this.model.canplay = true;
-            }.bind( this ));
-
-            _.each( ["play", "pause", "timeupdate"], function( e ) {
-                jQuery(this.audio).on( e, function() {
-                    this.model.trigger( e, {
-                        layer: this.model,
-                        currentTime: this.audio.currentTime,
-                        duration: this.audio.duration
-                    });
-                }.bind( this ));
-            }, this );
-
-            // listen for volume changes
-            this.model.on("change:volume", this.onVolumeChange, this );
-        },
-
-        onVolumeChange: function( model, vol ) {
-            console.log('on vol change', vol );
-            if ( this.audio ) {
-                this.audio.volume = vol;
-            }
-        },
-
         verifyReady: function() {
-            this.setAudio();
-            this.$('audio').on("canplay", function() {
-                this.audio.pause();
-                this.model.trigger( "visual_ready", this.model.id );
+
+            this.audio = this.$("#audio-el-" + this.model.id )[0];
+
+            this.audio.load();
+            this.audio.addEventListener("canplaythrough", function() {
+                this.onCanPlay();
             }.bind( this ));
-        }
+        },
+
+        onCanPlay: _.once(function() {
+            this.audio.pause();
+            this.audio.currentTime = this.getAttr("cue_in");
+
+            if ( this.getAttr("cue_out") || this.getAttr("loop") ) {
+                this.listen();
+            }
+            this.model.trigger( "visual_ready", this.model.id );
+        }),
+
+        listen: _.once(function() {
+            // don't need to listen to audio time if there's no cue out!
+            if ( this.getAttr("cue_out") !== null ) {
+                this.audio.addEventListener("timeupdate", function(){
+                    var currentTime = this.audio.currentTime;
+
+                    if ( currentTime >= this.getAttr("cue_out" ) ) {
+                        if ( this.getAttr("loop") ) {
+                            this.audio.pause();
+                            this.audio.currentTime = this.getAttr("cue_in");
+                            this.audio.play();
+                        } else {
+                            this.audio.pause();
+                            this.audio.currentTime = this.getAttr("cue_in");
+                        }
+                    }
+                }.bind( this ));
+            }
+
+            this.audio.addEventListener("ended", function(){
+                if ( this.getAttr("loop") ) {
+                    this.audio.pause();
+                    this.audio.currentTime = this.getAttr("cue_in");
+                    this.audio.play();
+                } else {
+                    this.audio.pause();
+                    this.audio.currentTime = this.getAttr("cue_in");
+                }
+            }.bind( this ));
+        })
+
     });
 
     return Layer;
