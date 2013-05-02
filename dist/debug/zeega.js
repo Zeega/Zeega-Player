5f2df93e1f260d7b05d8187a0f21bb4e101353ba
jbergen <jbergen@gmail.com>
2013-05-02 16:41:38 -0700

    release

--- dist/debug/zeega.js
+++ dist/debug/zeega.js
@@ -132,7 +132,7 @@ __p+='<div class="modal-content">\n    <div class="modal-title">Edit your text</
 ( attr.content )+
 '</textarea>\n\n        <div class="textarea-info">max 140 characters</div>\n\n        <div class="text-controls clearfix">\n            <div class="color-selector">\n                <input class="simple-color" value="'+
 ( attr.color )+
-'"/>\n            </div>\n            <a href="#" class="btnz btnz-light text-btn-bold"><i class="icon-bold"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-italic"><i class="icon-italic"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-align-left"><i class="icon-align-left"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-align-center"><i class="icon-align-center"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-align-right"><i class="icon-align-right"></i></a>\n\n            <select class="font-list" style=""></select>\n\n            <div class="control-module">\n                <div class="control-title">font size</div>\n                <select class="size-list" style="">\n                    <option value="100">8</option>\n                    <option value="125">10</option>\n                    <option value="150">12</option>\n                    <option value="175">14</option>\n                    <option value="200">18</option>\n                    <option value="250">24</option>\n                    <option value="375">36</option>\n                    <option value="500">48</option>\n                    <option value="800">72</option>\n                    <option value="1600">144</option>\n                    <option value="2400">200</option>\n                    <option value="3600">300</option>\n                </select>\n            </div>\n\n            <div class="control-module">\n                <div class="control-title">line height</div>\n                <select class="line-height-list">\n                    <option value="1">1</option>\n                    <option value="1.25">1.25</option>\n                    <option value="1.5">1.5</option>\n                    <option value="1.75">1.75</option>\n                    <option value="2">2</option>\n                </select>\n            </div>\n\n            <div class="control-module">\n                <div class="control-title">text-position</div>\n                <select class="text-position-list">\n                    <option value="top">top</option>\n                    <option value="middle">middle</option>\n                    <option value="bottom">bottom</option>\n                </select>\n            </div>\n            \n        </div>\n\n        <div class="sample-header">sample</div>\n        <div class="text-sample">'+
+'"/>\n            </div>\n            <a href="#" class="btnz btnz-light text-btn-bold"><i class="icon-bold"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-italic"><i class="icon-italic"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-align-left"><i class="icon-align-left"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-align-center"><i class="icon-align-center"></i></a>\n            <a href="#" class="btnz btnz-light text-btn-align-right"><i class="icon-align-right"></i></a>\n\n            <select class="font-list" style=""></select>\n\n            <div class="control-module">\n                <div class="control-title">font size</div>\n                <select class="size-list" style="">\n                    <option value="100">8</option>\n                    <option value="125">10</option>\n                    <option value="150">12</option>\n                    <option value="175">14</option>\n                    <option value="200">18</option>\n                    <option value="250">24</option>\n                    <option value="375">36</option>\n                    <option value="500">48</option>\n                    <option value="800">72</option>\n                    <option value="1600">144</option>\n                    <option value="2400">200</option>\n                    <option value="3600">300</option>\n                </select>\n            </div>\n\n            <div class="control-module">\n                <div class="control-title">line height</div>\n                <select class="line-height-list">\n                    <option value="1">1</option>\n                    <option value="1.25">1.25</option>\n                    <option value="1.5">1.5</option>\n                    <option value="1.75">1.75</option>\n                    <option value="2">2</option>\n                </select>\n            </div>\n\n            <div class="control-module">\n                <div class="control-title">mobile position</div>\n                <select class="text-position-list">\n                    <option value="top">top</option>\n                    <option value="middle">middle</option>\n                    <option value="bottom">bottom</option>\n                </select>\n            </div>\n            \n        </div>\n\n        <div class="sample-header">sample</div>\n        <div class="text-sample">'+
 ( attr.content )+
 '</div>\n\n        <div class="bottom-chooser clearfix">\n            <a href="#" class="submit btnz btnz-submit">OK</a>\n        </div>\n    </div>\n</div>\n';
 }
@@ -31812,6 +31812,7 @@ function( Backbone, jquery, Spinner ) {
 
         attributes: {},
         parserPath: "app/zeega-parser/",
+        hasSoundtrack: false,
 
         gmapAPI: "waiting",
         spinner: new Spinner({
@@ -33202,7 +33203,6 @@ function( app, Controls ) {
     return app.Backbone.LayoutView.extend({
 
         className: function() {
-            console.log("classname", this)
             return "visual-element visual-" + this.model.get("type").toLowerCase();
         },
 
@@ -33329,6 +33329,7 @@ function( app, Controls ) {
         applyVisualProperties: function() {
             var mediaTargetCSS = {},
                 containerCSS = {};
+
             _.each( this.visualProperties, function( prop ) {
                 if ( _.contains( this.containerAttributes, prop ) ) {
                     containerCSS[ prop ] = this.getAttr( prop ) + ( this.units[ prop ] ? this.units[ prop ] : "" );
@@ -34872,7 +34873,6 @@ function( app, _Layer, Visual, TextModal ) {
 
                 this.$el.css(css );
             } else {
-                console.log("APPLY WRONG")
                 this.$el.css({
                     top: this.getAttr("top") + "%",
                     left: this.getAttr("left") + "%"
@@ -34981,7 +34981,7 @@ function( Zeega, LayerModel, Visual ) {
     Layer.Youtube.Visual = Visual.extend({
 
         template: "youtube/youtube",
-        //ignoreFirst: true,
+        ignoreFirst: true,
         afterRender: function(){
             if( /iPhone|iPod/i.test(navigator.userAgent) ) {
                 this.$(".youtube-player").addClass( "mobile" );
@@ -35016,13 +35016,10 @@ function( Zeega, LayerModel, Visual ) {
         },
 
         onStateChange: function(e){
-            // if( /iPad/i.test(navigator.userAgent) && e.data ==2 && this.ignoreFirst ) {
-            //     this.ignoreFirst = false;
-            //     this.ytPlayer.playVideo();
-            // }
-            // else
-
-            if (e.data == 2 || e.data == 5){
+            if( this.model.status.get("current_sequence_model").get("attr").soundtrack && /iPad/i.test(navigator.userAgent) && e.data ==2 && this.ignoreFirst ) {
+                this.ignoreFirst = false;
+                this.ytPlayer.playVideo();
+            } else if (e.data == 2 || e.data == 5){
                 if( /iPad/i.test(navigator.userAgent) ) {
                     this.$(".ipad-cover").removeClass("visible");
                 }
@@ -35033,7 +35030,9 @@ function( Zeega, LayerModel, Visual ) {
                 this.$(".play-button").fadeIn("fast");
                 
             } else if (e.data == 1 ){
-                
+                if( Zeega.mode == "player"){
+                    this.model.status.get("project").suspend();
+                }
                 this.$(".play-button").fadeOut("fast");
                 this.$(".youtube-player").addClass("active");
                
@@ -35076,6 +35075,9 @@ function( Zeega, LayerModel, Visual ) {
 
         onExit: function(){
             this.ytPlayer.pauseVideo();
+            if( Zeega.mode == "player"){
+                this.model.status.get("project").play();
+            }
         }
 
     });
@@ -35131,6 +35133,8 @@ function( app, Layers ) {
 
     return app.Backbone.Model.extend({
 
+        soundtrackModel: null,
+
         defaults: {
             advance_to: null,
             attr: {
@@ -35154,10 +35158,16 @@ function( app, Layers ) {
         lazySave: null,
 
         initialize: function() {
-
             this.lazySave = _.debounce(function() {
                 this.save();
             }.bind( this ), 1000 );
+        },
+
+        initSoundtrackModel: function( layers ) {
+            if ( this.get("attr").soundtrack ) {
+                this.soundtrackModel = app.soundtrack = layers.get( this.get("attr").soundtrack );
+                this.soundtrackModel.status = app.status;
+            }
         },
 
         onFrameSort: function() {
@@ -35198,7 +35208,6 @@ function( app, Layers ) {
 
                 attr.soundtrack = newLayer.id;
                 this.set("attr", attr );
-                this.persistLayer( newLayer );
                 view.setSoundtrackLayer( newLayer );
 
                 this.lazySave();
@@ -35208,7 +35217,6 @@ function( app, Layers ) {
         removeSoundtrack: function( layer ) {
             var attr = this.get("attr");
 
-            this.unpersistLayer( layer );
             attr.soundtrack = false;
             this.set("attr", attr );
         },
@@ -35831,6 +35839,8 @@ function( app, SequenceModel, FrameCollection, LayerCollection, LayerModels ) {
                 sequence.frames.sequence = sequence;
                 sequence.frames.initLayers( layerCollection, options );
             });
+
+            this.at(0).initSoundtrackModel( layerCollection );
             // at this point, all frames should be loaded with layers and layer classes
         }
     });
@@ -35849,7 +35859,7 @@ function( app, SequenceCollection ) {
 
         defaults: {
             authors: null,
-            cover_image: null,
+            cover_image: "",
             date_created: null,
             date_published: null,
             date_updated: null,
@@ -35868,6 +35878,23 @@ function( app, SequenceCollection ) {
             user_id: null
         },
 
+        defaultCoverImages: [
+            "http://giphy.com/gifs/VxbP9tLeKzazm",
+            "http://giphy.com/gifs/4lLVnnMbawnss",
+            "http://giphy.com/gifs/bq6gi8shRqgyA",
+            "http://giphy.com/gifs/BDqInV6xYl1Ju",
+            "http://giphy.com/gifs/k0ywWCPu4IlEs",
+            "http://giphy.com/gifs/Mi6gE0Qjw2dWM",
+            "http://www.musicobsessed.com/wp-content/gallery/sfmo/tv-set-funky-cuteo.gif",
+            "http://24.media.tumblr.com/tumblr_m8582nac7y1r0k830o1_500.gif",
+            "http://24.media.tumblr.com/tumblr_lnxkb8K8u61qjvkx9o1_500.gif",
+            "http://25.media.tumblr.com/tumblr_mbjwlvwO5R1reeolao1_500.gif",
+            "http://reignandpour.com/home_files/tv.gif",
+            "http://s8.favim.com/orig/72/gif-animated-gif-tv-static-glitch-Favim.com-687367.gif",
+            "http://alaingiffard.files.wordpress.com/2007/12/applaudissements-001_1173713587.gif",
+            "http://www.poly.edu/sites/polyproto.poly.edu/files/cinemaNOISE.gif"
+        ],
+
         defaultOptions: {
             preloadRadius: 2,
             attach: {}
@@ -35878,6 +35905,9 @@ function( app, SequenceCollection ) {
         },
 
         initialize: function( data, options ) {
+            // if ( this.get("cover_image") == "" ) {
+            //     this.set("cover_image", this.defaultCoverImages[ Math.floor( Math.random() * this.defaultCoverImages.length ) ])
+            // }
             this.options = _.defaults( options, this.defaultOptions );
             this.parser = options.parser;
             this.parseSequences();
@@ -36119,7 +36149,6 @@ function( app, SequenceCollection ) {
         publishProject: function() {
 
             if ( this.get("date_updated") != this.get("date_published") || this.updated ) {
-                var mobile = this.validateMobile();
                 
                 this.updated = false;
                 this.once("sync", this.onProjectPublish, this);
@@ -36129,7 +36158,7 @@ function( app, SequenceCollection ) {
                 }
                 this.save({
                     publish_update: 1,
-                    mobile: mobile
+                    mobile: true
                 });
                 console.log("already published. published again");
             } else {
@@ -36139,48 +36168,6 @@ function( app, SequenceCollection ) {
 
         onProjectPublish: function( model, response ) {
             this.set({ publish_update: 0 });
-        },
-
-        validateMobile: function() {
-            var layers, validLayerTypes, maxAudioLayers, valid;
-            
-            layers = [];
-            validLayerTypes = ["Image", "Audio", "Text", "Link", "Rectangle"];
-            maxAudioLayers = 1;
-            maxFrames = null;
-            valid = true;
-
-
-            this.sequences.each(function( sequence ) {
-
-                if ( maxFrames !== null && ( maxFrames -= sequence.frames.length ) < 0 ) {
-                    valid = false;
-                    return false;
-                }
-
-                sequence.frames.each(function( frame ) {
-                    frame.layers.each(function( layer ) {
-
-                        var layerTypeValid = _.contains( validLayerTypes, layer.get("type") );
-
-                        if ( !layerTypeValid ) {
-                            valid = false;
-                            return false;
-                        }
-
-                        // dupe layer. ignore
-                        if ( !_.contains( layers, layer.id ) && layer.get("type") == "Audio" && maxFrames-- < 0 ) {
-                            layers.push( layer.id );
-                            valid = false;
-                            return false;
-                        } else if ( !_.contains( layers, layer.id ) && layer.get("type") == "Audio" ) {
-                            layers.push( layer.id );
-                        }
-                    });
-                });
-            });
-
-            return valid;
         }
 
     });
@@ -37425,6 +37412,7 @@ function( app, ZeegaParser, Relay, Status, PlayerLayout ) {
             this._mergeAttributes( attributes );
             this.relay = new Relay.Model();
             this.status = new Status.Model({ project: this });
+            app.status = this.status; // booooo
 
             this._setTarget();
             this._load( attributes );
@@ -37583,12 +37571,16 @@ function( app, ZeegaParser, Relay, Status, PlayerLayout ) {
             var currentFrame = this.status.get("current_frame"),
                 startFrame = this.get("startFrame"),
                 isCurrentNull, isStartNull;
+
+            this.loadSoundtrack();
+
             if ( !this.ready ) {
                 this.render(); // render the player first!
             } else if ( this.state == "paused" || this.state == "suspended" ) {
                 this._fadeIn();
                 if ( currentFrame ) {
                     this.state = "playing";
+                    app.soundtrack.play();
                     this.status.emit( "play", this );
                     this.status.get("current_frame_model").play();
                 }
@@ -37611,12 +37603,20 @@ function( app, ZeegaParser, Relay, Status, PlayerLayout ) {
             }
         },
 
+        loadSoundtrack: _.once(function() {
+            app.soundtrack.on("layer_ready", function() {
+                app.soundtrack.play();
+            });
+            app.soundtrack.render();
+        }),
+
         // if the player is playing, pause the project
         pause: function() {
             if ( this.state == "playing" ) {
                 this.state ="paused";
                 // pause each frame - layer
                 this.status.get("current_frame_model").pause();
+                app.soundtrack.pause();
                 // pause auto advance
                 this.status.emit("pause");
             }
