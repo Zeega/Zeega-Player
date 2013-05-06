200a1249380d6d4019f93773129369913997e140
jbergen <jbergen@gmail.com>
2013-05-06 14:24:52 -0700

    updated endpage

--- dist/debug/zeega.js
+++ dist/debug/zeega.js
@@ -35158,7 +35158,7 @@ function( app, Layer, Visual ){
 
     L.EndPageLayer.Visual = Visual.extend({
 
-        // template: "end_page/endpage",
+        template: "end_page/endpage",
 
         visualProperties: [
             "height",
