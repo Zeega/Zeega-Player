127ffda8f911d8da28c9550909271e27a6886f2f
jbergen <jbergen@gmail.com>
2013-05-06 14:27:10 -0700

    release

--- dist/debug/zeega.js
+++ dist/debug/zeega.js
@@ -74,13 +74,7 @@ return __p;
 this["JST"]["app/zeega-parser/plugins/layers/end_page/endpage.html"] = function(obj){
 var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
-__p+='<!-- <div class="endpage-inner">\n    <img class="end-logo" src="assets/img/zeega-red-white-250.png">\n\n    <div class="user-block user-block-large">\n        <a href="http://zeega.com/user/'+
-( user_id )+
-'" target="blank">\n            <div class="profile-token"\n                style="\n                    background: url('+
-( userThumbnail )+
-');\n                    background-size: cover;\n                "\n            ></div>\n            <div class="profile-name">by '+
-( authors )+
-'</div>\n        </a>\n    </div>\n\n    <div class="share-block">\n        <ul class="share-sites">\n            <li><a href="#"><i class="endpage-social endpage-social-twitter"></i></a></li>\n            <li><a href="#"><i class="endpage-social endpage-social-facebook"></i></a></li>\n            <li><a href="#"><i class="endpage-social endpage-social-tumblr"></i></a></li>\n        </ul>\n    </div>\n\n</div> -->';
+__p+='';
 }
 return __p;
 };
