8725422d7669b099fd62101c50b4db4f61d198ef
James Burns <jameburn@gmail.com>
2013-06-05 12:16:11 -0700

    mongo updates

--- app/modules/controls-view.js
+++ app/modules/controls-view.js
@@ -63,7 +63,7 @@ function( Zeega, ArrowView, CloseView, PlayPauseView ) {
 
        
         onFramePlay: function( info ) {
-
+            console.log(info)
             if( this.model.status.get("frameHistory").length > 1 ){
                 this.activateArrow("ZEEGA-prev");
             } else {
